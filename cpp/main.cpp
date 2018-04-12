21430fe2e81d7dbc5c93ee15d3f785f4f1020f96
mickuc <michalxkuczaj@gmail.com>
2018-04-12 13:15:46 +0200

    test GIT atom

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -12,7 +12,7 @@ int main() {
   // cout << dfs;
 
 
-  BFS bfs(test->stops, test->stops[0]->returnId() );
+//  BFS bfs(test->stops, test->stops[0]->returnId() );
  // bfs(test->stops, test->stops[0]->returnId() );
 
   return 0;
