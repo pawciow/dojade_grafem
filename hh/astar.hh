3bf236787a678b75b5b3fcc029d5758ea223969e
mickuc <michalxkuczaj@gmail.com>
2018-04-17 20:45:29 +0200

    nie bangla

--- hh/astar.hh
+++ hh/astar.hh
@@ -1,21 +1,24 @@
-// #ifndef HH_ASTAR_HH_
-// #define HH_ASTAR_HH_
-//
-// #include "../hh/loadbinary.hh"
-// #include <functional>
-// #include "interfaces.hh"
-// #include <queue>
-// #include <math.h>
-//
-//
-// //priority_queue<int, Stop, std::greater<int> > q2 > Q;
-// //std::priority_queue<Stop ,int, std::greater<int> > Q;
-//
-//
-// class aStar
-// {
-// 	map<int, double> distances;
-// 	vector<Stop*> stops;
-// };
-//
-// #endif /* HH_ASTAR_HH_ */
+#ifndef HH_ASTAR_HH_
+#define HH_ASTAR_HH_
+
+#include "../hh/loadbinary.hh"
+#include "interfaces.hh"
+#include <functional>
+#include <math.h>
+#include <map>
+#include <queue>
+
+// priority_queue<int, Stop, std::greater<int> > q2 > Q;
+// std::priority_queue<Stop ,int, std::greater<int> > Q;
+
+class aStar {
+  std::map<int, double> distances;
+  vector<Stop*> stops;
+
+public:
+  aStar(vector<Stop*> stopp);
+  void calculateHeuristic(Stop *stop);
+  void findPath(Stop* start, Stop* stop);
+};
+
+#endif /* HH_ASTAR_HH_ */
