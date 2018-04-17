67220b0e13c8bddcf1f13790b937f898512a13c0
PawcioW <pawciow123@gmail.com>
2018-04-17 10:44:24 +0200

    wizualny update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -12,21 +12,21 @@ void printPath(int vectorNumbBegin, int vectorNumbEnd, vector<Stop*> Vect)
 	string stop = Vect[vectorNumbBegin]->returnStopName();
 	string begin = Vect[vectorNumbEnd]->returnStopName();
 	//cout << begin << endl;
-	bool STOP = false;
+	bool STOP = true;
 	while(STOP)
 	{
 		for(auto& It: Vect)
 		{
-			if(It->returnStopName() == begin)
+			if(It->returnStopName() == stop)
 			{
 				cout << begin << endl;
-				begin = It->previous;
+				stop = It->previous;
 			}
-			/*if(It->previous == stop) Bo nie dziala, trzeba dopracowac
+			if(It->returnStopName() == begin) //Bo nie dziala, trzeba dopracowac
 			{
 				STOP = false;
 				break;
-			}*/
+			}
 		}
 
 	}
@@ -37,7 +37,7 @@ void printPath(int vectorNumbBegin, int vectorNumbEnd, vector<Stop*> Vect)
 
 int main() {
 	const int DESTINATION_TEST = 10; //dziwne, niektore przystanki nie dzialaja w algorytmie
-	const int FROM_TEST = 2;
+	const int FROM_TEST = 3;
   cout <<'\n'<< "Start:" << endl;
   LoadData *test = new LoadData();// ładuje w konstruktorze
 
@@ -55,7 +55,7 @@ int main() {
   BFS bfs(test->stops, FROM_TEST );
   bfs(test->stops, FROM_TEST ); // przystanek nr 21 nie dziala??
   cout << "bfs done \n";
- // cout << bfs;
+  //cout << bfs;
 
   cout  <<"Going to: "<< test->stops[DESTINATION_TEST]->returnStopName()
 		<< " from: "   << test->stops[FROM_TEST]->returnStopName() << endl;
