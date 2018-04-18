5c050c7bd1ebebb67301bce8d3cb42f50b9979e0
PawcioW <pawciow123@gmail.com>
2018-04-18 23:18:42 +0200

    mam cholerna sciezke

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,69 +8,56 @@
 using namespace std;
 
 
-void printPath(const int vectorNumbBegin,const int vectorNumbEnd,const vector<Stop*> & Vect)
+auto printPath(const Stop* from,const Stop* destination, const vector<Stop*> & Vect)
 {
-	string stop = Vect[vectorNumbBegin]->returnStopName();
-	string begin = Vect[vectorNumbEnd]->returnStopName();
-	int dupa= 0 ;
-	bool STOP = true;
-	while(STOP)
+	for(auto&& e: Vect)
 	{
-		for(auto& It: Vect)
-		{
-			if(It->returnStopName() == begin) // Bo nie dziala, trzeba dopracowac
+		if(e->stop_name == destination->previous)
 			{
-				STOP = false;
-				break;
+				cout << endl << e->stop_name;
+				return e;
 			}
-
-			if(It->returnStopName() == stop)
-			{
-				dupa++;
-				cout << It->returnStopName() << endl;
-				stop = It->previous;
-			}
-		}
 	}
-	cout << dupa << endl;
 
 }
+void reconstructPath(const Stop* from,const Stop* destination, const vector<Stop*> & Vect)
+{
+	cout << "Sciezki:" << endl << destination->stop_name;
+
+	auto e = destination;
+	bool X = true;
+	while(X)
+	{
+		e = printPath(from, e, Vect);
+		if (e == from)
+			X = false;
+
+	}
+}
+
 
 int main() {
 
 	const int FROM_TEST = 1;
 	const int DESTINATION_TEST = 15; //dziwne, niektore przystanki nie dzialaja w algorytmie
 
-  cout <<'\n'<< "Start:" << endl;
-  LoadData *test = new LoadData();// ładuje w konstruktorze
+	cout <<'\n'<< "Start:" << endl;
+	LoadData *test = new LoadData();// ładuje w konstruktorze
 
-   DFS dfs(test->stops);
-   dfs(test->stops, test->stops[FROM_TEST]);
+	DFS dfs(test->stops);
+	dfs(test->stops, test->stops[FROM_TEST]);
 
-   cout << "Going to: " << test->stops[DESTINATION_TEST]->returnStopName();
-   //test->stops[DESTINATION_TEST]->print_stop_specific();
-   cout << " from:"		<< test->stops[FROM_TEST]->returnStopName();
-  // test->stops[FROM_TEST]->print_stop_specific();
-   cout << "\nPath is:" << test->stops[DESTINATION_TEST]->previous <<" \n";
+	reconstructPath(test->stops[FROM_TEST], test->stops[DESTINATION_TEST], test->stops);
 
-
-   printPath(FROM_TEST,DESTINATION_TEST,test->stops);
-
-  printPath(FROM_TEST, DESTINATION_TEST, test->stops);
-	aStar astar(test->stops);
+	/*aStar astar(test->stops);
 	astar.findPath(test->stops[FROM_TEST], test->stops[DESTINATION_TEST]);
-	astar.printPath();
+	astar.printPath();*/
 	//BĘDĘ MUSIAŁ DODAĆ NAZWY LINI KTÓRYMI SIE JEDZIE
 
   /*BFS bfs(test->stops);
   bfs(test->stops, test->stops[FROM_TEST] ); // przystanek nr 21 nie dziala??
   cout << "bfs done \n";
   //cout << bfs;
-
-  cout << "Going to: "   << test->stops[DESTINATION_TEST]->returnStopName()
-	   << " from: "   	 << test->stops[FROM_TEST]->returnStopName();
-  cout << " \n Path is:" << test->stops[FROM_TEST]->previous << endl;
-
   printPath(FROM_TEST,DESTINATION_TEST,test->stops);*/
 
   return 0;
